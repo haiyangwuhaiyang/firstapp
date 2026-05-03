610b690ab548754d0b155e90983146f5ebe26846
haiyangwuhaiyang <286948197@qq.com>
2026-05-03 14:46:17 +0800

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-st.title("入门演示")
-st.header("一级标题")
-st.subheader("二级标题")
+st.title("好好好")
+st.header("尝试一个网页")
+st.subheader("开始吧")
 # 引入文字
 st.write("高高兴兴一家人")
 st.write("欢迎小王")
-st.image("./第一节/图片/吴海洋本科学位.jpg")
+st.image("./第一节/cat.jpg")
 password = st.text_input("请输入姓名")
 st.write(f"您输入的姓名:{password}")
 
